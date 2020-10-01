bcde8b0b42c917649c4fd15aa47922e2c211fcce
ShengHua Wu <swu1@wayfair.com>
2020-10-01 15:54:08 +0200

    Append content for chapter types and functions.

--- types-and-functions/exercise.hs
+++ types-and-functions/exercise.hs
@@ -1,3 +1,5 @@
+-- http://book.realworldhaskell.org/read/types-and-functions.html
+
 -- False :: Bool
 -- (["foo", "bar"], 'a') :: ([[Char]], Char) 
 -- [(True, []), (False, [['a']])] :: [(Bool, [[Char]])]
@@ -7,3 +9,8 @@ add a b = a + b
 myDrop n xs = if n <= 0 || null xs
               then xs
               else myDrop (n - 1) (tail xs)
+
+lastButOne :: [a] -> Maybe a
+lastButOne [] = Nothing
+lastButOne [_] = Nothing
+lastButOne xs = Just $ last . take 2 . reverse $ xs
