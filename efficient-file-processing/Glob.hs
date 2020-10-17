ff357e624bf1b8961eeb5e9b2d62ec400d39ddef
ShengHua Wu <swu1@wayfair.com>
2020-10-17 18:13:10 +0200

    First attempt of glob pattern.

--- efficient-file-processing/Glob.hs
+++ efficient-file-processing/Glob.hs
@@ -5,19 +5,41 @@ module Glob
         ,   testCases
         ) where
 
-type Text = String
-type Pattern = String
+import Control.Applicative
+import Data.Char
+
+-- Match any one char
+any :: String -> (Bool, String)
+any []     = (False, [])
+any (x:xs) = (True, xs)
+
+-- Match one given char
+one :: Char -> String -> (Bool, String)
+one c s@(x:xs) 
+    | x == c    = (True, xs)
+    | otherwise = (False, s)
+
+-- Match any one or more chars until encountering a given char
+oneOrMoreUntil :: Char -> String -> (Bool, String)
+oneOrMoreUntil c s
+    | result && x == c = (True, s)
+    | result           = oneOrMoreUntil c xs 
+    | otherwise        = (False, s) 
+    where
+        (result, (x:xs)) = Glob.any s
+
 
 -- Match a text with a given pattern
-match :: Text -> Pattern -> Bool
-match [] [] = True
-match [] _  = False
-match _ []  = False
-match (t:ts) (p:ps)
-    | p == '*'  = True && match ts ps -- !!!: This doesn't work
-    | p == '?'  = match ts ps 
+match :: String -> String -> (Bool, String)
+match [] []     = (True, [])
+match [] _      = (False, [])
+match input []  = (False, input)
+match input@(t:ts) pattern@(p:ps)
+    | p == '*' && ps == [] = (True, [])
+    | p == '*' && ps /= [] = oneOrMoreUntil (head ps) input -- !!!: This is not correct
+    | p == '?'  = Glob.any input -- !!!: This is not correct
     | t == p    = match ts ps
-    | otherwise = False
+    | otherwise = (False, input)
 
 -- Tests
 testCases :: IO ()
