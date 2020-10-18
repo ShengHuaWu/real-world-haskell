abfb63c0f45785a04d43a0b5526213124fe05990
ShengHua Wu <swu1@wayfair.com>
2020-10-18 12:39:56 +0200

    Append exercise of glob pattern matching.

--- efficient-file-processing/Glob.hs
+++ efficient-file-processing/Glob.hs
@@ -13,42 +13,47 @@ any :: String -> (Bool, String)
 any []     = (False, [])
 any (x:xs) = (True, xs)
 
--- Match one given char
-one :: Char -> String -> (Bool, String)
-one c s@(x:xs) 
-    | x == c    = (True, xs)
-    | otherwise = (False, s)
+-- Match zero or more chars until encountering a given char
+zeroOrMoreUntil :: Char -> String -> (Bool, String)
+zeroOrMoreUntil _ [] = (False, [])
+zeroOrMoreUntil c input@(x:xs)
+    | x == c    = (True, input)
+    | otherwise = zeroOrMoreUntil c xs
+
+-- Concat one match with another match
+concat :: (String -> (Bool, String)) -> (String -> (Bool, String)) -> String -> (Bool, String)
+concat match anotherMatch input
+    | result    = anotherMatch rest
+    | otherwise = (result, rest)
+    where
+        (result, rest) = match input
 
 -- Match any one or more chars until encountering a given char
 oneOrMoreUntil :: Char -> String -> (Bool, String)
-oneOrMoreUntil c s
-    | result && x == c = (True, s)
-    | result           = oneOrMoreUntil c xs 
-    | otherwise        = (False, s) 
-    where
-        (result, (x:xs)) = Glob.any s
+oneOrMoreUntil c input = Glob.any `Glob.concat` (zeroOrMoreUntil c) $ input
 
-
--- Match a text with a given pattern
+-- Give a pattern to match a text
 match :: String -> String -> (Bool, String)
 match [] []     = (True, [])
-match [] _      = (False, [])
-match input []  = (False, input)
-match input@(t:ts) pattern@(p:ps)
+match _ []      = (False, [])
+match [] input  = (False, input)
+match pattern@(p:ps) input@(t:ts) 
     | p == '*' && ps == [] = (True, [])
-    | p == '*' && ps /= [] = oneOrMoreUntil (head ps) input -- !!!: This is not correct
-    | p == '?'  = Glob.any input -- !!!: This is not correct
-    | t == p    = match ts ps
+    | p == '*' && ps /= [] = oneOrMoreUntil (head ps) `Glob.concat` (match ps) $ input
+    | p == '?'  = Glob.any `Glob.concat` (match ps) $ input
+    | t == p    = match ps ts
     | otherwise = (False, input)
 
 -- Tests
 testCases :: IO ()
 testCases = do
     print $ match "abc" "abc"
-    print $ match "abc" "cba"
-    print $ match "abc" "?bc"
-    print $ match "bc" "?bc"
-    print $ match "abc" "a*"
-    print $ match "b" "b*"
-    print $ match "abc" "*b*"
-    print $ match "ab" "*b*"
+    print $ match "cba" "abc"
+    print $ match "?bc" "abc" 
+    print $ match "?bc" "bc" 
+    print $ match "a*" "abc" 
+    print $ match "b*" "b" 
+    print $ match "*b" "ab" 
+    print $ match "*b" "b"
+    print $ match "*b*" "abc" 
+    print $ match "*b*" "ab" 
