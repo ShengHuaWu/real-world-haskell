3eae80352503e4007f2f7e0f8ffe06ae3d8e5465
ShengHua Wu <swu1@wayfair.com>
2020-10-29 15:10:53 +0100

    Append monoid and property tests.

--- data-structures/DList.hs
+++ data-structures/DList.hs
@@ -8,6 +8,8 @@ module Dlist (
   , dfoldr
 ) where
 
+import Test.QuickCheck
+
 -- `unDL` :: DList a -> [a] -> [a]
 newtype DList a = DL { unDL :: [a] -> [a] }
 
@@ -23,7 +25,7 @@ toList (DL xs) = xs [] -- xs :: [a] -> [a]
 empty :: DList a
 empty = DL id
 
--- equivalent of the list type's (:) operator
+-- Equivalent of the list type's (:) operator
 cons :: a -> DList a -> DList a
 cons x (DL xs) = DL ((x:) . xs) -- (x:) :: [a] -> [a] and xs :: [a] -> [a]
 
@@ -46,3 +48,22 @@ dmap f xs = fromList $ fmap f (toList xs)
 
 instance Functor DList where
     fmap = dmap
+
+-- `Dlist` is a Monoid
+instance Semigroup (DList a) where
+  xs <> ys = append xs ys
+
+instance Monoid (DList a) where
+  mempty = empty
+  mappend = append
+
+-- Properties tests
+-- Before running any properties test, `DList` has to conform `Show` & `Arbitrary`
+instance (Show a) => Show (DList a) where
+  show = show . toList
+
+instance (Arbitrary a) => Arbitrary (DList a) where
+  arbitrary = fromList <$> arbitrary -- The first `arbitrary` is `Gen (DList a)` and the second one is `Gen [a]`
+
+prop_mappend_mempty :: Ord a => DList a -> Bool
+prop_mappend_mempty xs = (toList $ mempty `mappend` xs) == (toList $ xs `mappend` mempty)
